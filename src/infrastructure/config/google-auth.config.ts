54a5f1573857608ef2456a97a2075cb46e4e0745
islom abdurahmonov <islomabdurakhmonov8@gmail.com>
2025-05-04 13:06:54 +0500

    fix: google-auth.config.ts redirect_uri updated

--- src/infrastructure/config/google-auth.config.ts
+++ src/infrastructure/config/google-auth.config.ts
@@ -3,5 +3,5 @@ import { serverConfig } from "./server.config";
 export const googleAuthConfig = {
     client_id:      process.env.GOOGLE_CLOUD_CLIENT_ID,
     client_secret:  process.env.GOOGLE_CLOUD_CLIENT_SECRET,
-    redirect_uri:   `${serverConfig.protocol}://api.${serverConfig.host}:${serverConfig.port}/v1/auth/google/callback`,
+    redirect_uri:   `${serverConfig.protocol}://api.${serverConfig.domain}/v1/auth/google/callback`,
 }
--- src/infrastructure/config/index.ts
+++ src/infrastructure/config/index.ts
@@ -1,4 +1,5 @@
 export * from './google-auth.config';
+export * from './https-ssl.config';
 export * from './pagination.config';
 export * from './server.config';
 export * from './token.config';
